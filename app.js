6d4b4f63db0de0ba82710e090286db50f1c1b6c6
Francisco belinchon <fbelinchon@gmail.com>
2024-08-15 18:11:48 +0200

    hasta lección 5

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import createHomepageTemplate from './views/index.js';
+import createListTemplate from './views/list.js';
+import BOOKS_DATA from './data/data.js';
 
 // create app
 const app = express();
@@ -9,8 +12,20 @@ app.use(express.static('public'));
 
 // routes
 app.get('/', (req, res) => {
-  res.send();
+  res.send(createHomepageTemplate());
 });
+
+app.get('/books', (req, res) => {
+  res.send(createListTemplate());
+})
+
+app.post("/books", (req, res)=>{
+  const {title,author} = req.body;
+  const id = Math.random().toString();
+  BOOKS_DATA.push({id,title,author});
+
+  res.send(`<li>${title},${author}</li>`)
+})
 
 // listen to port
 app.listen(3000, () => {
---
+++ views/index.js
@@ -0,0 +1,47 @@
+const createHomepageTemplate = () => /*html*/`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>My Reading List</title>
+      <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+      <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+      <header>
+        <h1>My Reading List</h1>
+      </header>
+
+      <main>
+        <div class="book-list">
+          <button 
+          hx-get="./books"
+          hx-target="closest div"
+          >
+          Muestra los libros</button>
+        </div>
+
+        <div class="add-book-form">
+          <h2>What do you want to read?</h2>
+          <form>
+            <input 
+              type="text"
+              name="title"
+              placeholder="title"
+            >
+            <input 
+              type="text"
+              name="author"
+              placeholder="author"
+            >
+            <button hx-post="/books"
+              hx-target=".book-list ul"
+              hx-swap="afterbegin"
+            >Añadir libro</button>
+          </form>
+        </div>
+      </main>
+    </body>
+  </html>
+`;
+
+export default createHomepageTemplate;
